0fa21c3947d7f18e117d2bed38dda006853ab748
SashoBG123 <136566780+SashoBG123@users.noreply.github.com>
2024-10-16 10:49:22 +0300

    Add files via upload

---
+++ Swpool/Controller/PoolActionController.cs
@@ -0,0 +1,37 @@
+﻿using Swpool.Model;
+using Swpool.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swpool.Controller
+{
+    internal class PoolActionController
+    {
+        // Обекти от класовете Display и Pool
+        Display currentDisplay;
+        Pool currentPool;
+
+        // Конструктор
+        public PoolActionController()
+        {
+            currentDisplay = new Display();
+            currentPool = new Pool();
+        }
+
+        // Метод за стартиране на процеса
+        public void Start()
+        {
+            currentDisplay.Input();
+            this.currentPool.Volume = this.currentDisplay.V;
+            this.currentPool.Pipe2FlowRate = this.currentDisplay.P2;
+            this.currentPool.Pipe1FlowRate = this.currentDisplay.P1;
+            this.currentPool.Hours = this.currentDisplay.H;
+            this.currentDisplay.TotalWater = this.currentPool.TotalWater();
+            currentDisplay.Output();
+
+        }
+    }
+}
---
+++ Swpool/Program.cs
@@ -0,0 +1,18 @@
+﻿using Swpool.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swpool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PoolActionController controller = new PoolActionController();
+            controller.Start();
+        }
+    }
+}
